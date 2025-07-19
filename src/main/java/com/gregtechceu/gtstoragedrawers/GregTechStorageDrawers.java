0f350ecebaa448a3e92c7569a681a95f529ff14a
Maya <10861407+serenibyss@users.noreply.github.com>
2025-07-19 10:41:17 -0600

    build files + skeleton mod

---
+++ src/main/java/com/gregtechceu/gtstoragedrawers/GregTechStorageDrawers.java
@@ -0,0 +1,16 @@
+package com.gregtechceu.gtstoragedrawers;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Mod(GregTechStorageDrawers.MODID)
+public class GregTechStorageDrawers {
+
+    public static final String MODID = "gtstoragedrawers";
+    public static final String MOD_NAME = "GregTech Storage Drawers";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
+
+    public GregTechStorageDrawers(FMLJavaModLoadingContext ctx) {}
+}
